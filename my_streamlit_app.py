690da935e3cacd1657088251c40a59de1528e3f8
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-05-07 17:44:52 +0200

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,3 +1,16 @@
+pip install matplotlib
+
+import subprocess
+
+# Install Streamlit using pip
+subprocess.call(['pip', 'install', 'streamlit'])
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import streamlit as st
 import pandas as pd
 import numpy as np
